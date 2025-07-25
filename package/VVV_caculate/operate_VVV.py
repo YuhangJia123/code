40d37b337ceaba137d62395394f1151519b597cf
JiaYuhang <jiayuhang0308@163.com>
2025-07-25 22:05:19 +0800

    add readin function in operato_VVV.py

--- package/VVV_caculate/operate_VVV.py
+++ package/VVV_caculate/operate_VVV.py
@@ -12,8 +12,37 @@ import sys
 from VVV_tools import VVV_cal
 
 # ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+#配套的读取文件
+def readin_VVV(phi_dir, t_source, Nev1, conf_id):#读取t时刻的phi_vdv
+
+    f = open("%s/VVV.t%03i.Px%iPy%iPz%i.conf%s" % (phi_dir, t_source, Px, Py, Pz, conf_id), 'rb')
+    phi_vdv = np.fromfile(f, dtype='f8')
+    Nev=int(np.cbrt(phi_vdv.size/2))
+    phi_vdv = phi_vdv.reshape(Nev, Nev, Nev, 2)
+    phi_vdv = phi_vdv[..., 0] + phi_vdv[..., 1] * 1j
+    phi_vdv = phi_vdv[0:Nev1, 0:Nev1, 0:Nev1]
+    f.close()
+    phi_vdv_cupy=cp.array(phi_vdv)
+    return phi_vdv_cupy
+
+def readin_VVV_all(phi_dir, Nt, Nev1, conf_id):#读取所有t时刻的phi_vdv
+
+    phi_vdv_all = np.zeros((Nt, Nev1, Nev1, Nev1), dtype=complex)
+
+    for t in range(Nt):
+        print("reading phi_vdv, t=%d" % t)
+        f = open("%s/VVV.t%03i.Px%iPy%iPz%i.conf%s" % (phi_dir, t, Px, Py, Pz, conf_id), 'rb')
+        VVV = np.fromfile(f, dtype='f8')
+        Nev=int(np.cbrt(VVV.size/2))
+        VVV = VVV.reshape(Nev, Nev, Nev, 2)
+        VVV = VVV[..., 0] + VVV[..., 1] * 1j
+        VVV = VVV[0:Nev1, 0:Nev1, 0:Nev1]
+        phi_vdv_all[t,:,:,:] =VVV
 
 
+    phi_vdv_cupy = cp.array(phi_vdv_all)
+    return phi_vdv_cupy
 
 # ------------------------------------------------------------------------------
 infile = fileinput.input()
